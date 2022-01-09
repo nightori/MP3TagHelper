579fe544b8e226a3456275072bee5faff3ef4b2f
Sergey Savin <nightori@yandex.ru>
2022-01-09 13:49:17 +0600

    Minor refactoring

--- src/mp3taghelper/Mp3Filter.java
+++ src/mp3taghelper/Mp3Filter.java
@@ -3,11 +3,11 @@ package mp3taghelper;
 import java.io.File;
 import java.io.FilenameFilter;
 
-public class Mp3Filter implements FilenameFilter{
+public class Mp3Filter implements FilenameFilter {
     @Override
-    public boolean accept(File dir, String name){
+    public boolean accept(File dir, String name) {
         String[] nsp = name.split("\\.");
-        String ext = nsp[nsp.length-1];
+        String ext = nsp[nsp.length - 1];
         return ext.equals("mp3");
     }
 }
